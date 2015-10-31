b5e472706983b44adce7e5dd406c121a933ebdcf
gerald-fongwe <geraldjam2004@yahoo.com>
2015-10-31 03:14:17 -0600

    created booostrap assignment file in php

--- index.php
+++ index.php
@@ -1,38 +1,112 @@
 <!DOCTYPE html>
-<html>
-	<html lang="en">
-		<head>
-			<meta charset="utf-8">
-			<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-			<meta name="viewport" content="width=device-width, initial-scale=1"/>
-			<!-- Bootstrap Latest compiled and minified CSS -->
-			<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
-					integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
-					crossorigin="anonymous">
-			<!-- Optional theme -->
-			<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
-					integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX"
-					crossorigin="anonymous">
-			<!-- Latest compiled and minified JavaScript -->
-			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
-					  integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
-					  crossorigin="anonymous"></script>
+<html lang="en" xmlns="http://www.w3.org/1999/html" xmlns="http://www.w3.org/1999/html">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 
-			<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
-			<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-			<!--[if lt IE 9]>
-			<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-			<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-			<![endif]-->
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css"
+				integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ=="
+				crossorigin="anonymous">
 
-			<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-			<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
-			<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-		</head>
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
+				integrity="sha384-aUGj/X2zp5rLCbBxumKTCw2Z50WgIr1vs/PFN4praOTvYXWlVyh2UtNUU0KAUhAX" crossorigin="anonymous">
 
-		<body>
-			<h1>Hello, world!</h1>
-		</body>
+		<!--  MY CUSTOM CSS GOES HERE!!! -->
+		<!-- <link rel="stylesheet" href="/css/master.css"> -->
 
-	</html>
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+		<script type="text/javascript"
+				  src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/additional-methods.min.js"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"
+				  integrity="sha512-K1qjQ+NcF2TYO/eI3M6v8EiNYZfA95pQumfvcVrTHtwQVDG+aHRqLi/ETn2uB+1JqwYqVG3LIvdm9lj6imS/pQ=="
+				  crossorigin="anonymous"></script>
+
+		<
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+		<!-- <script type="text/javascript" src="custom.js"></script> -->
+		<title>Bootstrap Exercise</title>
+	</head>
+
+	<body>
+		<nav class="navbar navbar-default" role="navigation">
+				<a div class="navbar-brand" href="#">GET SMART WEB</a>
+
+				<div class="navbar-header">
+
+					<ul class="nav navbar-nav">
+						<li><a href="index.html">Home</a></li>
+						<li><a href="index.html">About</a></li>
+						<li><a href="index.html">Career</a></li>
+						<li><a href="index.html">Help</a></li>
+						<li><a href="index.html">Contact</a></li>
+					</ul>
+				</div>
+				<div class="footer">
+
+				</div>
+			</div>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>The UN is planning a program for food distribution to a remote area of central Asia
+							following a natural disaster. They would like to know how communities will respond to
+							the distribution of food, and what effect the disaster relief may have on the community.
+							The sociocultural anthropologist would be best suited to studying the community and the effects
+							of food distribution on the community?!</h2>
+					</div>
+					<div class="row">
+						<div class="col-md-3">
+							<h2> Anthropology is the study of culture as a complex system that requires study of many
+								diverse component parts. Studying other cultures requires that one live with the subjects of
+								!</h2>
+						</div>
+						<div class="row">
+							<div class="col-md-3">
+								<h2>their study.Holism describes a specific type of fieldwork that involves immersion in a
+									culture,
+									and taking on aspects of the community being studied to better understand it. </h2>
+							</div>
+							<div class="row">
+								<div class="col-md-3">
+									<h2>A fossil is any trace or impression of an organism that has been preserved in the earth's
+										crust from past geologic time.Dead animal materials are most likely to NOT become
+										fossilized.</h2>
+								</div>
+								<div class="row">
+									<div class="col-md-3">
+										<h2> Physical anthropologists study human biology including growth patterns,
+											diet, disease, skeletal anatomy, and demographics.</h2>
+									</div>
+									<div class="row">
+										<div class="col-md-6">
+											<h2>  The ABO blood groups in humans are controlled by three alleles of a single gene.
+												O is recessive while A and B are codominant (an individual with both the A and B
+												alleles will have the blood type.</h2>
+										</div>
+										<div class="row">
+											<div class="col-md-6">
+												<h2>  AB rather than A or B). If a woman who has type O blood gives birth to a
+													child with type O blood could herhusband,who has type AB blood, be the father of
+													this child?! The answer is NO!!!</h2>
+											</div>
+										</div>
+		</nav>
+
+	</body>
+
+</html>
