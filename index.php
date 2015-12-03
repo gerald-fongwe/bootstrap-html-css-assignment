52a392d6410aa50c97cf49d5f30ed70ce671ee36
gerald-fongwe <geraldjam2004@yahoo.com>
2015-12-03 13:44:14 -0700

    added pink and blue background to the bootstrap assignment

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 		<!-- Latest compiled and minified CSS -->
 		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"/>
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css" rel="stylesheet"/>
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css"
+				rel="stylesheet"/>
 		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 		<!-- <link rel="stylesheet" href="/css/master.css"> -->
 
@@ -15,19 +16,21 @@
 		<![endif]-->
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/js-cookie/2.0.2/js.cookie.min.js"></script>
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
-		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script>
-		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/additional-methods.min.js"></script>
+		<script type="text/javascript"
+				  src="//cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
+		<script type="text/javascript"
+				  src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/jquery.validate.min.js"></script>
+		<script type="text/javascript"
+				  src="//cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.14.0/additional-methods.min.js"></script>
 		<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		<title>Bootstrap Exercise</title>
 	</head>
-
-	<body>
-		<div class="header">
-			<div class="container">
+	<div class="header">
+		<div class="container">
 			<a class="logo" href="#">GET SMART WEB</a>
+
 			<div class="nav">
-					<ul>
+				<ul>
 					<li><a href="#">Home</a></li>
 					<li><a href="#">About</a></li>
 					<li><a href="#">Career</a></li>
@@ -35,51 +38,89 @@
 					<li><a href="#">Contact</a></li>
 				</ul>
 			</div>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">
-						<p>The UN is planning a program for food distribution to a remote area of central Asia
-							following a natural disaster. They would like to know how communities will respond to
-							the distribution of food, and what effect the disaster relief may have on the community.
-							The sociocultural anthropologist would be best suited to studying the community and the effects
-							of food distribution on the community!</p>
+			<main>
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12">
+							<div class="text-box bg-blue">
+								<h1>The Study Of Anthropology</h1>
+								<p>The UN is planning a program for food distribution to a remote area of central Asia
+									following a natural disaster. They would like to know how communities will respond to
+									the distribution of food, and what effect the disaster relief may have on the community.
+									The sociocultural anthropologist would be best suited to studying the community and the
+									effects
+									of food distribution on the community!</p>
+							</div>
+						</div>
 					</div>
+
 					<div class="row">
 						<div class="col-md-3">
-							<p> Anthropology is the study of culture as a complex system that requires study of many
-								diverse component parts. Studying other cultures requires that one live with the
+							<div class="text-box bg-pink">
+								<h3>The Class is Full</h3>
+
+								<p>Anthropology is the study of culture as a complex system that requires study of many
+									diverse component parts. Studying other cultures requires that one live with the
 								</p>
+							</div>
 						</div>
-						<div class="row">
-							<div class="col-md-3">
-								<p>subject of their study. Holism describes a specific type of fieldwork that involves immersion in a
+						<div class="col-md-3">
+							<div class="text-box bg-pink">
+								<h3>The Class is Full</h3>
+
+								<p>subject of their study. Holism describes a specific type of fieldwork that
+									involves immersion in a
 									culture, and taking on aspects of the community being studied to better</p>
 							</div>
-							<div class="row">
-								<div class="col-md-3">
-									<p>understand it. A fossil is any trace or impression of an organism that has been preserved in the earth's
-										crust from past geologic time. Dead animal materials are most</p>
-								</div>
-								<div class="row">
-									<div class="col-md-3">
-										<p>likely to NOT to become fossilized. Physical anthropologists study human biology including growth patterns,
-											diet, disease, skeletal anatomy, and demographics.</p>
-									</div>
-									<div class="row">
-										<div class="col-md-6">
-											<p>The ABO blood groups in humans are controlled by three alleles of a single gene.
-												O is recessive, while A and B are codominant (an individual with both the A and B
-												alleles will have the blood type</p>
-										</div>
-										<div class="row">
-											<div class="col-md-6">
-												<p> AB rather than A or B). If a woman who has type O blood gives birth to a
-													child with type O blood could herhusband,who has type AB blood, be the father of
-													this child?! The answer is NO!!!</p>
-												<div class="footer">
-											</div>
-										</div>
+						</div>
+						<div class="col-md-3">
+							<div class="text-box bg-pink">
+								<h3>The Class is Full</h3>
 
-	</body>
+								<p>understand it. A fossil is any trace or impression of an organism that
+									has been preserved in the earth's
+									crust from past geologic time. Dead animal materials are most</p>
+							</div>
+						</div>
+						<div class="col-md-3">
+							<div class="text-box bg-pink">
+								<h3>The Class is Full</h3>
 
+								<p>likely to NOT to become fossilized. Physical anthropologists
+									study human biology including growth patterns,
+									diet, disease, skeletal anatomy, and demographics.</p>
+							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="text-box bg-pink">
+								<h3>The Class is Full</h3>
+
+								<p>The ABO blood groups in humans are controlled by three
+									alleles of a single gene.
+									O is recessive, while A and B are codominant (an
+									individual with both the A and B
+									alleles will have the blood type</p>
+							</div>
+						</div>
+
+						<div class="col-md-6">
+							<div class="text-box bg-pink">
+								<h3>Cliche Waistcoat</h3>
+
+								<p> AB rather than A or B). If a woman who has
+									type O blood gives birth to a
+									child with type O blood could her husband,who
+									has type AB blood, be the father of
+									this child?! The answer is NO!!!</p>
+							</div>
+						</div>
+			</main>
+		</div>
+		<div class=" footer">
+			<div class="container">
+				<p>Test.</p>
+			</div>
+		</div>
+	</div>
 </html>
